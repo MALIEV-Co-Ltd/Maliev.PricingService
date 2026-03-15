041adb1f8b062f3e9197b16a472b08d645ee3dd8
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-03-15 22:00:52 +0700

    fix: remove duplicate AddDbContext registration that overrode Aspire connection string

--- Maliev.PricingService.Infrastructure/DependencyInjection.cs
+++ Maliev.PricingService.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,5 @@
 using Maliev.PricingService.Infrastructure.Persistence;
 using Maliev.PricingService.Application.Interfaces;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,9 +9,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<Persistence.PricingDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-
         services.AddScoped<IPricingDbContext>(provider => provider.GetRequiredService<Persistence.PricingDbContext>());
         services.AddScoped<Services.HistoricalMigrationService>();
 
